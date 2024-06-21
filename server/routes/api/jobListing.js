0c8a7b4ac06ca19a49deb2bbad01d950d3f810ad
jadayoubi <jadayoubi1977@gmail.com>
2024-06-21 17:32:54 +0300

    added admin career portal to add and delete careers & added linked the career to the baclend adnd added the detials page

---
+++ server/routes/api/jobListing.js
@@ -0,0 +1,20 @@
+// routes/api/jobListing.js
+
+const express = require('express');
+const router = express.Router();
+const jobListingController = require('../../controllers/jobListing');
+const verifyToken = require('../../middleware/authMiddleware'); // Adjust the path as per your application
+
+// Apply the middleware to the routes
+// router.post('/', verifyToken, jobListingController.createJobListing);
+// router.get('/', verifyToken, jobListingController.getAllJobListings);
+// router.get('/:id', verifyToken, jobListingController.getJobListingById);
+// router.put('/:id', verifyToken, jobListingController.updateJobListing);
+// router.delete('/:id', verifyToken, jobListingController.deleteJobListing);
+router.post('/',verifyToken, jobListingController.createJobListing);
+router.get('/', jobListingController.getAllJobListings);
+
+// DELETE a job listing by ID
+router.delete('/:id',verifyToken, jobListingController.deleteJobListing);
+    
+module.exports = router;
