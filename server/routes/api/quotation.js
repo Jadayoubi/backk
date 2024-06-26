1bda5083f3e6b6b377280acf1339d822bf92855f
jadayoubi <jadayoubi1977@gmail.com>
2024-06-26 12:14:52 +0300

    fixed the bug with the web dev admin side

--- server/routes/api/quotation.js
+++ server/routes/api/quotation.js
@@ -4,13 +4,9 @@ const quotationController = require('../../controllers/quotationController');
 const verifyToken = require('../../middleware/authMiddleware');
 
 // Log each request to this router
-router.use((req, res, next) => {
-  console.log(`Received request: ${req.method} ${req.url}`);
-  next();
-});
 
 // Routes for web quotations
-router.get('/webQuotations', verifyToken, quotationController.getAllQuotations);
+router.get('/webQuotations',verifyToken, quotationController.getAllQuotations);
 router.post('/web', quotationController.submitWebQuotation);
 router.put('/webQuotations/:id', verifyToken, quotationController.updateQuotationStatus);
 router.delete('/webQuotations/:id', verifyToken, quotationController.deleteQuotation);
